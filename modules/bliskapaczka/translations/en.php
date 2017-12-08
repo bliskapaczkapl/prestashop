ddf048be89951da0765bdadd886fca2a1b776153
Mateusz Koszutowski <mkoszutowski@divante.pl>
2017-12-08 13:30:00 +0100

    Fixing CS

--- modules/bliskapaczka/translations/en.php
+++ modules/bliskapaczka/translations/en.php
@@ -50,38 +50,75 @@ $_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_1d850fe4dde4180715bb412
     = 'Select delivery point';
 $_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_965904ec74865a04088695d82a656bb0']
     = 'Selected delivery point';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_66f51e43606e2baebb8e9c3bfe965da4'] = 'Shipping:';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_6f05787682585c32498e88bcd2ea88fc'] = 'Delivery methods';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_3818429ccff49b9d6ab2b4a9a137a00a'] = 'No carrier is needed for this order.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_cc9b9807af9192bed55cb1e92ba28844'] = 'Choose a shipping option for this address:';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_8fb37535c12fbe2899ab442ec474c157'] = 'Choose a shipping option';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_2a08bd3364d511e7573f0ebdbeda7c3d'] = 'Delivery time:';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_b6dddfff5178ce64364ca3f683930354'] = 'The best price and speed';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_c2a32e7734b79d54eaaddb651707b403'] = 'The fastest';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_b1992d8f7c58180c1ebd461b7d502423'] = 'The best price';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_21034ae6d01a83e702839a72ba8a77b0'] = '(tax excl.)';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_1f87346a16cf80c372065de3c54c86d9'] = '(tax incl.)';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_b24ce0cd392a5b0b8dedc66c25213594'] = 'Free';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_23753c5016cd82e4ec63ba61cd2a13f5'] = 'For this product:';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_3d113dda516dec82953c7ef1b770278c'] = 'For these products:';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_2d51d96d770cc2244be31a85e608de19'] = 'No carriers available.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_0383858ad9a560ca2ecb8066c1681807'] = 'The product selection cannot be delivered by the available carrier(s): it is too heavy. Please amend your cart to lower its weight.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_942649e42a59a56bd717acc3fed307b8'] = 'The product selection cannot be delivered by the available carrier(s). Please amend your cart.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_7c013b3defbf4cec9cd69f2e99a8d84c'] = 'The product selection cannot be delivered by the available carrier(s): its size does not fit. Please amend your cart to reduce its size.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_a6cc8cbe3fa07d0fca172c36a603332c'] = 'No carriers available for the address "%s".';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_bd5b551d1757a856d9f2d01dc7850080'] = 'Leave a message';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_d93651d012e77dac5581b08db33195b2'] = 'If you would like to add a comment about your order, please write it in the field below.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_6407498d5c2b5cdd00f58568a4835db3'] = 'Recyclable Packaging';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_9193b58520051d8b59ca64a5cc5353ff'] = 'I would like to receive my order in recycled packaging.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_0d9175fe89fb80d815e7d03698b6e83a'] = 'Gift';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_32224bd6487699ea66652d3dc502455d'] = 'I would like my order to be gift wrapped.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_5c2dfd55fea1f5c6adef500fcb6403b4'] = 'Additional cost of';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_bf46d59ab4c5096d169c1a5eb39cc0c1'] = 'If you\'d like, you can add a note to the gift:';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_ad88a6c9e8c880f23639321d78ec859d'] = 'I agree to the terms of service and will adhere to them unconditionally.';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_d53afd7ef9af5405df040482add9b153'] = '(Read the Terms of Service)';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_dd1f775e443ff3b9a89270713580a51b'] = 'Previous';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_300225ee958b6350abc51805dab83c24'] = 'Continue shopping';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_7e0bf6d67701868aac3116ade8fea957'] = 'Proceed to checkout';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_deb10517653c255364175796ace3553f'] = 'Product';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_068f80c7519d0528fb08e82137a72131'] = 'Products';
-$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_5508a53851720a210e50f7b8a6f66f46'] = 'You must agree to the terms of service before continuing.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_66f51e43606e2baebb8e9c3bfe965da4']
+    = 'Shipping:';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_6f05787682585c32498e88bcd2ea88fc']
+    = 'Delivery methods';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_3818429ccff49b9d6ab2b4a9a137a00a']
+    = 'No carrier is needed for this order.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_cc9b9807af9192bed55cb1e92ba28844']
+    = 'Choose a shipping option for this address:';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_8fb37535c12fbe2899ab442ec474c157']
+    = 'Choose a shipping option';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_2a08bd3364d511e7573f0ebdbeda7c3d']
+    = 'Delivery time:';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_b6dddfff5178ce64364ca3f683930354']
+    = 'The best price and speed';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_c2a32e7734b79d54eaaddb651707b403']
+    = 'The fastest';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_b1992d8f7c58180c1ebd461b7d502423']
+    = 'The best price';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_21034ae6d01a83e702839a72ba8a77b0']
+    = '(tax excl.)';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_1f87346a16cf80c372065de3c54c86d9']
+    = '(tax incl.)';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_b24ce0cd392a5b0b8dedc66c25213594']
+    = 'Free';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_23753c5016cd82e4ec63ba61cd2a13f5']
+    = 'For this product:';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_3d113dda516dec82953c7ef1b770278c']
+    = 'For these products:';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_2d51d96d770cc2244be31a85e608de19']
+    = 'No carriers available.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_0383858ad9a560ca2ecb8066c1681807']
+    = 'The product selection cannot be delivered by the available carrier(s): it is too heavy. '
+    . 'Please amend your cart to lower its weight.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_942649e42a59a56bd717acc3fed307b8']
+    = 'The product selection cannot be delivered by the available carrier(s). Please amend your cart.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_7c013b3defbf4cec9cd69f2e99a8d84c']
+    = 'The product selection cannot be delivered by the available carrier(s): its size does not fit. '
+    . 'Please amend your cart to reduce its size.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_a6cc8cbe3fa07d0fca172c36a603332c']
+    = 'No carriers available for the address "%s".';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_bd5b551d1757a856d9f2d01dc7850080']
+    = 'Leave a message';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_d93651d012e77dac5581b08db33195b2']
+    = 'If you would like to add a comment about your order, please write it in the field below.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_6407498d5c2b5cdd00f58568a4835db3']
+    = 'Recyclable Packaging';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_9193b58520051d8b59ca64a5cc5353ff']
+    = 'I would like to receive my order in recycled packaging.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_0d9175fe89fb80d815e7d03698b6e83a']
+    = 'Gift';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_32224bd6487699ea66652d3dc502455d']
+    = 'I would like my order to be gift wrapped.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_5c2dfd55fea1f5c6adef500fcb6403b4']
+    = 'Additional cost of';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_bf46d59ab4c5096d169c1a5eb39cc0c1']
+    = 'If you\'d like, you can add a note to the gift:';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_ad88a6c9e8c880f23639321d78ec859d']
+    = 'I agree to the terms of service and will adhere to them unconditionally.';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_d53afd7ef9af5405df040482add9b153']
+    = '(Read the Terms of Service)';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_dd1f775e443ff3b9a89270713580a51b']
+    = 'Previous';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_300225ee958b6350abc51805dab83c24']
+    = 'Continue shopping';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_7e0bf6d67701868aac3116ade8fea957']
+    = 'Proceed to checkout';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_deb10517653c255364175796ace3553f']
+    = 'Product';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_068f80c7519d0528fb08e82137a72131']
+    = 'Products';
+$_MODULE['<{bliskapaczka}default-bootstrap>order-carrier_5508a53851720a210e50f7b8a6f66f46']
+    = 'You must agree to the terms of service before continuing.';
