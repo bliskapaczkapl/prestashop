7c45bda11d821ddca459b89fd5d03900cb1f0f7f
Paweł Karbowniczek <pkarbowniczek@divante.pl>
2020-01-13 12:40:53 +0100

    Added console.log

--- modules/bliskapaczka/views/js/bliskapaczka.js
+++ modules/bliskapaczka/views/js/bliskapaczka.js
@@ -3,7 +3,7 @@ function Bliskapaczka()
 }
 
 Bliskapaczka.showMap = function (operators, googleMapApiKey, testMode) {
-    console.log('Show');
+    console.log('Show m');
     aboutPoint = document.getElementById('bpWidget_aboutPoint');
     aboutPoint.style.display = 'none';
 
