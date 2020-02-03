599e2bd58f77521368dc048daba56c9b7429dc50
Paweł Karbowniczek <pkarbowniczek@divante.pl>
2020-02-03 15:13:05 +0100

    Added new line - for travis

--- modules/bliskapaczka/Bliskapaczka/Prestashop/Core/Mapper/MapperAbstract.php
+++ modules/bliskapaczka/Bliskapaczka/Prestashop/Core/Mapper/MapperAbstract.php
@@ -123,6 +123,7 @@ abstract class MapperAbstract
         if (in_array($order->pos_operator, self::OPERATORS_WITH_INSURANCE)) {
             $data['parcel']['insuranceValue'] = $order->total_paid + $order->total_shipping;
         }
+        
         return $data;
     }
 }
